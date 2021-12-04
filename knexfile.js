6cb63b7fb90f40ad8c4118da7bbfe70fff2d97f2
rafaelmenezz <faelsmenezes@gmail.com>
2021-12-04 01:00:24 -0300

    Add knex client

--- knexfile.js
+++ knexfile.js
@@ -1,37 +1,16 @@
 
-//  Update with your config settings.
-const { db } = require('./.env')
+// Update with your config settings.
+
 
 module.exports = {
-
-  client:{ 
-  development: {
-    client: 'postgresql',
-    version: '8.6.0',
-    connection: db,
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
+  client: 'postgresql',
+  version: '8.6.0',
+  connection: process.env.DATABASE_URL,
+  pool: {
+    min: 2,
+    max: 10
   },
-  production: {
-    client: 'postgresql',
-    version: '8.6.0',
-    connection: process.env.DATABASE_URL,
-    ssl: {
-      rejectUnauthorized: false
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
+  migrations: {
+    tableName: 'knex_migrations'
   }
-}
-
 };
