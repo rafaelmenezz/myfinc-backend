b9ab13b92e9d3caa32e2139426e4a704401aecff
rafaelmenezz <faelsmenezes@gmail.com>
2021-12-03 23:50:15 -0300

    Atualizado Knex

--- knexfile.js
+++ knexfile.js
@@ -1,16 +1,33 @@
- 
+
 // Update with your config settings.
 const { db } = require('./.env')
 
 module.exports = {
-  client: 'postgresql',
-  version: '8.6.0',
-  connection: db,
-  pool: {
-    min: 2,
-    max: 10
+
+  development: {
+    client: 'postgresql',
+    version: '8.6.0',
+    connection: db,
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
   },
-  migrations: {
-    tableName: 'knex_migrations'
+  production: {
+    client: 'postgresql',
+    version: '8.6.0',
+    connection: DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
   }
+
+
 };
